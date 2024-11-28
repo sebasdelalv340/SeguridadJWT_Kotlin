20615ea838d8582f8d1ed6a5e9d5a7d55cf1413a
Lainezz <diegolinaresmojacar@gmail.com>
2024-11-28 12:43:26 +0100

    arreglo

--- src/main/kotlin/com/es/jwtSecurityKotlin/model/Usuario.kt
+++ src/main/kotlin/com/es/jwtSecurityKotlin/model/Usuario.kt
@@ -1,10 +1,14 @@
 package com.es.jwtSecurityKotlin.model
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
+@Entity
+@Table(name = "usuarios")
 data class Usuario(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
